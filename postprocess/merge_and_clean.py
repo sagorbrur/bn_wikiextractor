5d74ac2cb225551c83c9b879e14a513cdb566251
Sagor Sarker <sagorhem3532@gmail.com>
2019-11-05 13:29:24 +0600

    Create merge_and_clean.py

---
+++ postprocess/merge_and_clean.py
@@ -0,0 +1,29 @@
+import glob
+import re
+
+def merge(files):
+    cleanr = re.compile('<.*?>')
+    cleantext=""
+    for file in files:
+        f = open(file, 'r').read()
+        clean = re.sub(cleanr, '', f) # remove tags
+        text = re.sub(r'[A-Za-z]', '', clean) # remove english
+        cleantext+=text+"\n"
+    return cleantext
+
+def clean_sentences(cleantext):
+    sentences = re.sub("[.,।!?\\-]", '\n', cleantext).split('\n') # remove punctuation and split to sentences
+    sentences = [x for x in sentences if x] # remove empty strings
+    return sentences
+    
+    
+if __name__=="__main__":
+    
+    files = glob.glob("out/*/*")
+    cleantext = merge(files)
+    sentences = clean_sentences(cleantext)
+    output = open("bn_wiki_sentences.txt", 'w')
+    for line in sentences:
+        output.write(line+"\n")
+
+    output.close()
